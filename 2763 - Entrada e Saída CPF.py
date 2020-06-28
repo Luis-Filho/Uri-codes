a7c9eeb8ef64bdc5b69d3ce0073ef26c66462783
Luis Costa <luis.filho@outlook.com.br>
2020-06-28 20:47:29 -0300

    2749, 2755, 2763 and 2764 added

---
+++ 2763 - Entrada e Saída CPF.py
@@ -0,0 +1,7 @@
+for i in input().split('.'):
+    if len(i) > 3:
+        print(i[0:3])        
+        print(i[4:])
+    else:
+        print(i)
+    
---
+++ 2862 - Inseto!.py
@@ -0,0 +1,5 @@
+for i in range(0, int(input())):
+    if int(input()) > 8000:
+        print("Mais de 8000!")
+    else:
+        print("Inseto!")
